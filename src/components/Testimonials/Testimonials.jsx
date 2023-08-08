41888a0ef27fee7ed5151f00211bfeb7739d6429
Mukesh Pilane <mukeshpilane@gmail.com>
2023-08-08 19:37:27 +0530

    update

--- src/components/Testimonials/Testimonials.jsx
+++ src/components/Testimonials/Testimonials.jsx
@@ -13,47 +13,35 @@ export default function Testimonials() {
       id: 1,
       img: "../../assets/images/slide1.jpg",
       text:
-        "“If you want to lay the best foundation possible, I’d recommend taking this course. The depth the instructors go into is incredible. I now feel so confident about starting up as a professional developer.”",
-      name: "John Tarkpor",
-      role: "Junior Front-end Developer",
-      portrait: John,
+        `“Forming TAPAS was the best decision of my college days. Introducing a totally non-technical concept to a technical community was our biggest challenge. To add on to our challenges Principal Sir Dr. Sandeep Joshi Sir used to always present us with challenges. Living upto his expectations and proving that we are here to stay, we will leave behind a team which will sustain and carry forward our dream was our sheer pleasure. 
+        Learning everything from scratch about committee formation, documentation, responsibilities, leadership qualities, etc etc.....we had a complete personality development by the end. 
+        Winning the title "National Youth Ambassador" award by competing with 180+ colleges nationwide was a humbling experience. 
+        
+        Always grateful to pillars of TAPAS, Dr. Nivedita Shreyans ma'am and preceptor & mentor Prof. Madhura Vyavahare ma'am without whose guidance and encouragement we would have been lost. 
+        
+        Love to see TAPAS committee generations working hard bringing in peace to the campus. Their trips and other initiatives in colleges makes me immensely happy and proud. Proud of all the generations of TAPAS. Thank you for carrying forward our dream.”`,
+      name: "Resmi Ramakrishnan",
+      role: "Former TAPAS member",
+      portrait: Tanya,
     },
     {
       id: 2,
       img: "../../assets/images/slide2.jpg",
       text:
-        "“I’ve been interested in coding for a while but never taken the jump, until now. I couldn’t recommend this course enough. I’m now in the job of my dreams and so excited about the future.”",
-      name: "Tanya Sinclair",
-      role: "UX Engineer",
-      portrait: Tanya,
+        "“It's been a wonderful time with Tapas, even today after graduating from college 2 years back. The lively events and the peace of mind still reminds me back of the days when we had just started out Tapas with 3 day Dhyanotsav. Looking back in time, the growth of Tapas is unmatched and may it bring peace to all students of our college!”",
+      name: "romax Rajeev",
+      role: "Former TAPAS member",
+      portrait: John,
     },
     {
       id: 3,
       img: "../../assets/images/slide3.jpg",
       text:
-        "“Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla eu lectus eu lectus luctus tempus vitae ac tortor. Fusce ac orci quis risus vulputate condimentum. Sed in nunc non ex auctor tincidunt. In faucibus tellus auctor tincidunt. Nulla facilisi.”",
-      name: "Lorem Ipsum",
-      role: "Developer",
-      portrait: "",
-    },
-    {
-      id: 4,
-      img: "../../assets/images/slide4.jpg",
-      text:
-        "“Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla eu lectus eu lectus luctus tempus vitae ac tortor. Fusce ac orci quis risus vulputate condimentum. Sed in nunc non ex auctor tincidunt. In faucibus tellus auctor tincidunt. Nulla facilisi.”",
-      name: "Lorem Ipsum",
-      role: "Designer",
-      portrait: "",
-    },
-    {
-      id: 5,
-      img: "../../assets/images/slide2.jpg",
-      text:
-        "“Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla eu lectus eu lectus luctus tempus vitae ac tortor. Fusce ac orci quis risus vulputate condimentum. Sed in nunc non ex auctor tincidunt. In faucibus tellus auctor tincidunt. Nulla facilisi.”",
-      name: "Lorem Ipsum",
-      role: "Tester",
-      portrait: "",
-    },
+        `“TAPAS has been a transformative experience in my college life. It's a remarkable committee that brought adventure, connections, and personal growth. The unique freedom within TAPAS allows seamless interaction with peers, juniors, seniors, and faculty. Collaborating with faculty for event planning enhances responsibility and decision-making skills. The connections made with people from various backgrounds become lasting through Heartfulness. My advice to future members is to actively participate, create memories, and seek support. TAPAS isn't just a committee; it's a family where connections thrive. ❤️”`,
+      name: "Vivek yadav",
+      role: "",
+      portrait: John,
+    }
   ];
 
   const totalSlides = slides.length;
